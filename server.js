7173828b4771a9c3f97729be9f3cef5b2034c1ce
Maria Gozdawa-Gołębiowska <m.gozdawa-golebiowska@gmail.com>
2024-09-01 13:48:10 +0200

    add client files

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const { v4: uuidv4 } = require('uuid'); 
 const db = require('./db');
+const cors = require("cors");
 
 const app = express();
 const testimonialRoutes = require('./routes/testimonials.routes');
 const seatsRoutes = require('./routes/seats.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
